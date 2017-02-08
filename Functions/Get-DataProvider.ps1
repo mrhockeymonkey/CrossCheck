a004d6660e37925d4a5834cdd486b0e7cecf95ec
mrhockeymonkey <s.a.matthews@live.co.uk>
2017-02-08 11:46:47 +0000

    actually fixed filtering this time

--- Functions/Get-DataProvider.ps1
+++ Functions/Get-DataProvider.ps1
@@ -19,19 +19,22 @@ Function Get-DataProvider {
 	$Path = Resolve-Path $ConfigDocument
 	Write-Verbose "Reading: $Path"
 	$ProviderConfig = Get-Content -Path $Path -Raw -ErrorAction Stop | ConvertFrom-Json 
-	
-	#Filter unwanted providers is user specifies name
-	If ($PSBoundParameters.ContainsKey('Name')) {
-		$ProviderConfig = $ProviderConfig | Where-Object {$_.Config.Name -like $PSBoundParameters['Name']}
-	}
 
 	#Loop through config and instatiate the required provider class for each
 	$ProviderConfig | ForEach-Object {
+		#Save the provider name for use in another scope
 		$ThisProvider = $_.Provider
 
 		#A defined provider may have 1 or more configs nested underneath, so loop through
 		$_.Config | ForEach-Object {
 			
+			#Filter unwanted providers if user specified name
+			If ($PSBoundParameters.ContainsKey('Name')) {
+				If ($_.Name -notlike $PSBoundParameters['Name']) {
+					Return
+				}
+			}
+
 			#Get the values of each config property
 			#Note definitions must respect the correct order of arguments for this to work
 			$Arguments = $_.psobject.properties.value
