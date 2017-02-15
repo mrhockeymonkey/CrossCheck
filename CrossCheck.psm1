b030a6d94754032e7fc9444ee2f93f62234679fe
mrhockeymonkey <s.a.matthews@live.co.uk>
2017-02-15 21:01:56 +0000

    splitting out classes again

--- CrossCheck.psm1
+++ CrossCheck.psm1
@@ -1,75 +1,14 @@
+#Load classes
+using module .\Issue.psm1
+using module .\Provider.psm1
+
 #Add script level variables
 New-Variable -Name ModuleRoot -Value $PSScriptRoot -Scope script
 
 #Add alias'
 New-Alias -Name Check -Value Invoke-Check -Force
 
-#Define Enums
-Enum Priority {
-	High
-	Medium
-	Low
-}
-
-<#
-	.SYNOPSIS
-	Abstract Class: Provider
-
-	.DESCRIPTION
-	This abstract class provides the basis from which providers should inherit. One cannot instantiate this
-	class directly. Provider plugins should implement every define method. Providers are responsible for 
-	retreiving datafrom external sources, caching that data for local use
-#>
-class Provider {
-	[String]$Provider
-	[String]$Name
-	[String]$Source
-
-	Provider([String]$Provider, [String]$Name, [String]$Source) {
-		#Throw if trying to instantiate this class
-		$Type = $this.GetType()
-		If ($Type -eq [Provider]){ #is
-			throw "$Type is an abstract class and must be inherited"
-		}
-		$this.Provider = $Provider
-		$this.Name     = $Name
-		$this.Source   = $Source
-	}
-
-	[PSObject]GetData() {
-		throw "Method GetData not implemented for provider $($this.ProviderName)"
-	}
-
-	[Void]CacheData() {
-		$this.GetData() | Export-Clixml -Path "$PSScriptRoot\Cache\$($this.Name).xml" -Force
-	}
-
-	[PSObject]GetCachedData() {
-		$CachedData = Import-Clixml -Path "$PSScriptRoot\Cache\$($this.Name).xml"
-		return $CachedData
-	}
-}
-
-<#
-	.SYNOPSIS
-	Standard Class: Issue
-
-	.DESCRIPTION
-	An issue is the object raised as the result of a check. 
-#>
-class Issue {
-	[String]$Title
-	[Priority]$Priority
-	[String]$Message
-
-	Issue([String]$Title, [Priority]$Priority) {
-		$this.Title = $Title
-		$this.Priority = $Priority
-	}
-}
-
-
-#Load Providers
+#Load plugin providers
 Try {
 	Get-ChildItem "$PSScriptRoot\Providers\*.ps1" | ForEach-Object {
 		$Item = $_
