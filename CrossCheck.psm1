e9f026c8f367defc9ca8356fce4b7cc3b18043a5
mrhockeymonkey <s.a.matthews@live.co.uk>
2017-02-24 12:25:26 +0000

    moving back to dotsourcing as debug was broken

--- CrossCheck.psm1
+++ CrossCheck.psm1
@@ -2,36 +2,27 @@
 using module .\Issue.psm1
 using module .\Provider.psm1
 
-#Add script level variables
-New-Variable -Name ModuleRoot -Value $PSScriptRoot -Scope script
-
-#Add alias'
-New-Alias -Name Check -Value Invoke-Check -Force
-
 #Load plugin providers
 Try {
 	Get-ChildItem "$PSScriptRoot\Providers\*.ps1" | ForEach-Object {
-		$Item = $_
-		Write-Debug "Loading Function: $($Item.BaseName)"
-		# Dot-sourcing in this way is faster as it skips validation of a ps1 file
-		. ([scriptblock]::Create([io.file]::ReadAllText($Item.FullName)))
+		. $_.FullName
 	}
 }
 Catch {
-	Write-Warning "Could not load $($Item.Name): $($_.Exception.Message)"
+	Write-Warning "Could not load provider: $($_.Exception.Message)"
 	Continue
 }
 
 #Load Functions
 Try {
 	Get-ChildItem "$PSScriptRoot\Functions\*.ps1" | ForEach-Object {
-		$Item = $_
-		Write-Debug "Loading Function: $($Item.BaseName)"
-		# Dot-sourcing in this way is faster as it skips validation of a ps1 file
-		. ([scriptblock]::Create([io.file]::ReadAllText($Item.FullName)))
+		. $_.FullName
 	}
 }
 Catch {
-	Write-Warning "Could not load $($Item.Name): $($_.Exception.Message)"
+	Write-Warning "Could not load function: $($_.Exception.Message)"
 	Continue
 }
+
+#Add alias'
+New-Alias -Name Check -Value Invoke-Check -Force
