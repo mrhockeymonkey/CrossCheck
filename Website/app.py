d3afc28a7dd3ccc2cf272d02754159fdfa7a038a
Scott Matthews <s.a.matthews@live.co.uk>
2017-04-05 22:17:13 +0100

    lots of website improvements

--- Website/app.py
+++ Website/app.py
@@ -11,15 +11,43 @@ def get_data():
 	data = json.loads(json_data)
 	return data
 
+# pages
 @app.route('/')
 def index():
 	return render_template('index.html')
 
-@app.route('/_get_issues_overview')
-def _get_issues_overview():
+@app.route('/checkdetail')
+def test():
+	title = request.args.get('title')
+	return render_template('checkdetail.html', title = title)
+
+
+# API
+@app.route('/_issues')
+def _issues():
 	# get the data and any arguments passed
 	data = get_data()
-	pri = request.args.get('priority')
+	title = request.args.get('title')
+	priority = request.args.get('priority')
+
+	if not title and not priority:
+		return jsonify(data)
+	elif title:
+		filtered_data = [elem for elem in data if elem['Title'] == title]
+		return jsonify(filtered_data)
+	elif priority:
+		# here we need to convert pri to int as it was passed in as a string
+		filtered_data = [elem for elem in data if elem['Priority'] == int(priority)]
+		return jsonify(filtered_data)
+	# note i am missing the case where both args are given. But with the current data structure
+	# this will just result in the same data returned anyway. 
+
+
+@app.route('/_issuesGroupedByTitle')
+def _get_issues_overview():
+	data = get_data()
+	title = request.args.get('title')
+	priority = request.args.get('priority')
 	
 	# loop through each object and group it based on its title
 	groups = defaultdict(list)
@@ -32,13 +60,16 @@ def _get_issues_overview():
 	# count is the length of the grouped objects c[1]
 	overview = [{'Title': c[0], 'Priority': c[1][0]['Priority'], 'Count': len(c[1])} for c in groups.items()]
 
-	if pri:
-		filtered = [elem for elem in overview if elem['Priority'] == int(pri)]
+	if priority:
+		filtered = [elem for elem in overview if elem['Priority'] == int(priority)]
+		return jsonify(filtered)
+	elif title:
+		filtered = [elem for elem in overview if elem['Title'] == title]
 		return jsonify(filtered)
 	else:
 		return jsonify(overview)
 
-@app.route('/_get_issues_count')
+@app.route('/_issuesGroupedByPriority')
 def _get_issues_count():
 	groups = defaultdict(list)
 	data = get_data()
@@ -49,20 +80,6 @@ def _get_issues_count():
 	count = [{'Priority':c[0] , 'Count': len(c[1])} for c in groups.items()]
 	count.append({'Priority': -1, 'Count': len(data) })
 	return jsonify(count)
-
-@app.route('/_get_issues_detail')
-def _get_issues_detail():
-	# get theargument for priority, /get_issues?priority=0
-	pri = request.args.get('priority')
-	
-	# get the data and filter it if pri was specified
-	data = get_data()
-	if pri:
-		# here we need to convert pri to int as it was passed in as a string
-		filtered_data = [elem for elem in data if elem['Priority'] == int(pri)]
-		return jsonify(filtered_data)
-	else:
-		return jsonify(data)
 
 
 @app.route('/_get_fake_data')
